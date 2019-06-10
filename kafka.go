2d1b653ef8c849082b849a725a09b07438a29db9
craqs <craqzz@gmail.com>
2019-06-10 13:37:57 +0200

    Remove ":"

--- kafka.go
+++ kafka.go
@@ -53,7 +53,7 @@ func NewKafkaAdapter(route *router.Route) (router.LogAdapter, error) {
 	var err error
 	var tmpl *template.Template
 	if text := os.Getenv("KAFKA_TEMPLATE"); text != "" {
-		tmpl, err := template.New("kafka").Funcs(funcs).Parse(text)
+		tmpl, err = template.New("kafka").Funcs(funcs).Parse(text)
 
 		if err != nil {
 			return nil, errorf("Couldn't parse Kafka message template. %v", err)
